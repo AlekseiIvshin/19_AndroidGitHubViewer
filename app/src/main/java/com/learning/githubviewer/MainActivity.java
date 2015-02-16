31604b80cbeeecdc2407c3da461ce9f2253d5b7b
Aleksei Ivshin <ivshinalekseim@mail.ru>
2015-02-16 14:27:11 +0400

    Add:Open-close drawer nevigation indicator

--- app/src/main/java/com/learning/githubviewer/MainActivity.java
+++ app/src/main/java/com/learning/githubviewer/MainActivity.java
@@ -10,9 +10,11 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
 import android.widget.ListView;
@@ -25,48 +27,88 @@ import java.net.URI;
 public class MainActivity extends ActionBarActivity implements ListViewFragment.OnRepositorySelectedListener {
 
     private static final String CURRENT_POSITION = "githubviewer.list.currentposition";
+
     private boolean mDualPane;
     private int mCurrentPos = -1;
+    private CharSequence mTitle;
+    private CharSequence mDrawerTitle;
 
+
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.v("MainActivity.onCreate","Creating");
-        mDualPane = (Configuration.ORIENTATION_LANDSCAPE == getResources().getConfiguration().orientation);
-        Log.v("MainActivity.onCreate","Landscape orientation: "+mDualPane);
 
+        mDualPane = (Configuration.ORIENTATION_LANDSCAPE == getResources().getConfiguration().orientation);
+        mTitle = mDrawerTitle = getTitle();
+
+//      Drawer Navigation
         ListView navigation = (ListView) findViewById(R.id.leftDrawer);
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.drawer_open, R.string.drawer_close) {
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                getSupportActionBar().setTitle(mTitle);
+                this.syncState();
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle(mDrawerTitle);
+                this.syncState();
+            }
+        };
+
+
         navigation.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.navigation)));
 
+        drawerLayout.setDrawerListener(mDrawerToggle);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        mDrawerToggle.syncState();
+
         if (findViewById(R.id.contentFrame) != null) {
-            Log.v("MainActivity.onCreate","Content frame not null");
-            if (savedInstanceState!=null || mCurrentPos>=0) {
+            if (savedInstanceState != null || mCurrentPos >= 0) {
                 return;
             }
-            Log.v("MainActivity.onCreate","current position = "+mCurrentPos+" Saved = "+savedInstanceState);
             ListViewFragment listViewFragment = new ListViewFragment();
 
             FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
 
             transaction.add(R.id.contentFrame, listViewFragment);
             transaction.commit();
-        } else{
-            Log.v("MainActivity.onCreate","Content frame not found(is null)");
         }
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putInt(CURRENT_POSITION,mCurrentPos);
+        outState.putInt(CURRENT_POSITION, mCurrentPos);
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        if(savedInstanceState!=null) {
+        if (savedInstanceState != null) {
             mCurrentPos = savedInstanceState.getInt(CURRENT_POSITION);
         }
     }
@@ -76,12 +118,6 @@ public class MainActivity extends ActionBarActivity implements ListViewFragment.
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -99,5 +135,15 @@ public class MainActivity extends ActionBarActivity implements ListViewFragment.
             transaction.replace(R.id.contentFrame, newDetailFragment).addToBackStack(null).commit();
 
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        // Handle your other action bar items...
+
+        return super.onOptionsItemSelected(item);
     }
 }
