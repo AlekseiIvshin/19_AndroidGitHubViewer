914102ec9daf434328ff5c8594b86a12c898813c
Aleksei Ivshin <ivshinalekseim@mail.ru>
2015-02-24 15:14:16 +0400

    Add:auto refresh

--- app/src/main/java/com/ivshinaleksei/githubviewer/ui/list/MyRecyclerViewAdapter.java
+++ app/src/main/java/com/ivshinaleksei/githubviewer/ui/list/MyRecyclerViewAdapter.java
@@ -10,6 +10,7 @@ import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -99,6 +100,7 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAd
         if (old != null) {
             old.close();
         }
+        notifyDataSetChanged();
     }
 
     // TODO: Add observers
@@ -140,6 +142,7 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAd
             repositoryLanguage.setText(item.language);
             repositoryName.setText(item.fullName);
         }
+
     }
 
 }
